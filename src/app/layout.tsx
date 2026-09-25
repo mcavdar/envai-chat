bb6a1036a2482405af661d9f241a1df87d923750
Mahmut CAVDAR <4072246+mcavdar@users.noreply.github.com>
2026-09-25 07:34:01 +0000

    updated test server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Agent Chat",
-  description: "Agent Chat UX by LangChain",
+  title: "Mento Chat",
+  description: "Mento Chat UX by EnvAI",
 };
 
 export default function RootLayout({
